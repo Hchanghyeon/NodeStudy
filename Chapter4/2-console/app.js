94e4534a2c06e0fb92ccb623f837114e7df8ffab
changhyeon <changhyeon>
2022-01-16 22:03:14 +0900

    1day

--- Chapter4/2-console/app.js
+++ Chapter4/2-console/app.js
@@ -18,7 +18,36 @@ console.assert(2 === 3, "not same");
 console.assert(2 === 2, "same!");
 
 // print object
-const student = { name: "ellie", age: 20 };
+const student = { name: "ellie", age: 20, company: { name: "AC" } };
 console.log(student);
 console.table(student); // 테이블 형태
-console.dir(student);
+console.dir(student, { showHidden: true, colors: false, depth: 0 });
+
+// measuring time
+// 시간 체크
+console.time("for loop");
+for (let i = 0; i < 10; i++) {
+  i++;
+}
+console.timeEnd("for loop");
+
+// counting
+function a() {
+  console.count("a function");
+}
+a();
+console.countReset("a function"); // 초기화
+a();
+
+// trace
+function f1() {
+  f2();
+}
+function f2() {
+  f3();
+}
+function f3() {
+  console.log("f3");
+  console.trace();
+}
+f1();
